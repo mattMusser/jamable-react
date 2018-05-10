2c076c9ec7ab8ac76401545402dcc4dd503e2cfe
Matt <webdevelopmentlyspeaking@gmail.com>
2018-05-10 15:22:44 -0500

    add landing content

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 const Landing = () => (
   <section className="landing">
-    Landing page will go here
+    <h1 className="hero-title">Turn up the jams!</h1>
+    
+    <section className="selling-points">
+      <div className="point">
+        <h2 className="point-title">Choose your music</h2>
+        <p className="point-description">If you want more options to choose from then use Spotify</p>
+      </div>
+      <div className="point">
+        <h2 className="point-title">Unlimited streaming and ad-free</h2>
+        <p className="point-description">No streaming limits or distractions.</p>
+      </div>
+      <div className="point">
+        <h2 className="point-title">Mobile Enabled</h2>
+        <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile browsers.</p>
+      </div>
+    </section>
   </section>
 );
 
