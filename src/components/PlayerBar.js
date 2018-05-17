9e7439c97b228a50c49e095b0da4fbbad7af139f
Matt <webdevelopmentlyspeaking@gmail.com>
2018-05-17 14:56:28 -0500

    add previous and skip buttons

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -4,7 +4,27 @@ class PlayerBar extends Component {
   render() {
     return (
       <section className="player-bar">
-        player bar goes here
+        <section id="buttons">
+          <button id="previous" onClick={this.props.handlePrevClick}>
+            <span className="fas fa-step-backward"></span>
+          </button>
+          <button id="play-pause" onClick={this.props.handleSongClick} >
+            <span className={this.props.isPlaying ? 'fas fa-pause' : 'fas fa-play'}></span>
+          </button>
+          <button id="next">
+            <span className="fas fa-step-forward" onClick={this.props.handleNextClick}></span>
+          </button>
+        </section>
+        <section id="time-control">
+          <div className="current-time">-:--</div>
+          <input type="range" className="seek-bar" value="0" />
+          <div className="total-time">-:--</div>
+        </section>
+        <section id="volume-control">
+          <div className="fas fa-volume-down"></div>
+          <input type="range" className="seek-bar" value="80" />
+          <div className="fas fa-volume-up"></div>
+        </section>
       </section>
     );
   }
